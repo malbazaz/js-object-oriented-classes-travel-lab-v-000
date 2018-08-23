d9257fb24b563e87833f29c257f7aaff8e112d28
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 15:22:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ class Driver{
 yearsExperienceFromBeginningOf(endDate){
   debugger;
   if(endDate>this.startDate.getFullYear()){
-return endDate - (this.startDate.getFullYear()-1)
+return endDate - this.startDate.getFullYear()
 }
 else{
 return this.startDate.getFullYear() - endDate
