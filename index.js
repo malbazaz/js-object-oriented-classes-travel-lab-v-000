e17b9d26ba79fed12684aa6f5a5be0146051b99b
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-23 15:57:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
+class Driver{
+    constructor(name,date){
+      this.name = name;
+      this.startDate = new Date(date);
+    }
 
+    yearsExperienceFromBeginningOf(endDate){
+      if(endDate>this.startDate.getFullYear()){
+          return endDate - this.startDate.getFullYear() -1
+        }
+      else{
+          return this.startDate.getFullYear() - endDate
+        }
+    }
+
+}
+
+class Route{
+
+    constructor(beginningLocation,endingLocation){
+      this.beginningLocation = beginningLocation;
+      this.endingLocation = endingLocation;
+    }
+
+    blocksTravelled(){
+      let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+      const hor = Math.abs(eastWest.indexOf(this.beginningLocation.horizontal) - eastWest.indexOf(this.endingLocation.horizontal))
+      const ver = Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical )
+      return hor + ver;
+    }
+
+    estimatedTime(bool=false){
+      if(bool === true ){
+        return (this.blocksTravelled())/2
+      }
+      else {
+        return (this.blocksTravelled())/3
+      }
+    }
+
+  }
